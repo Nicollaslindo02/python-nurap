4f51a986a1db053a5d373a50946859430a076ebf
Nicollaslindo02 <nicollaspipero@gmail.com>
2024-12-03 11:31:20 -0300

    Add files via upload

---
+++ aulatwo.py
@@ -0,0 +1,11 @@
+print("bom dia a todos presentes")
+
+nome_usuario = 'nicollas miranda' # tipo -> str = strir
+idade_usuario = 19 # tipo -> int interagir
+altura_usuario = 1.84 # tipo -> float = ponto fluente
+maior_idade = True # tipo -> bool boolean
+
+print('seja bem-vindo usuario')
+print('sua iadede é', idade_usuario, 'anos')
+print('voce tem', altura_usuario, "metros")
+print('voce é maior de idade?', maior_idade)
